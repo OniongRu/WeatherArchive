ac1a694c17d596dadb99ef439007006fee63ef48
OniongRu <light.smoky@yandex.ru>
2021-10-11 00:47:17 +0300

    Add alternative table type

--- WeatherArchive/Controllers/WeatherConditionsTableController.cs
+++ WeatherArchive/Controllers/WeatherConditionsTableController.cs
@@ -7,54 +7,55 @@ using WeatherArchive.ViewModels;
 
 namespace WeatherArchive.Controllers
 {
-    public class Month
+    public class DropDownList
     {
         public int Num { get; set; }
         public string Name { get; set; }
     }
 
-    public class Year
+    public class WeatherConditionsTableController : Controller
     {
-        public int YearNum { get; set; }
-        public string YearStr { get; set; }
-    }
-        public class WeatherConditionsTableController : Controller
-    {
-        private readonly IEnumerable<Month> _months = new List<Month>
+        private readonly IEnumerable<DropDownList> _months = new List<DropDownList>
         {
-            new Month { Num = 0, Name = "Месяц не выбран" },
-            new Month { Num = 1, Name = "Январь" },
-            new Month { Num = 2, Name = "Февраль" },
-            new Month { Num = 3, Name = "Март" },
-            new Month { Num = 4, Name = "Апрель" },
-            new Month { Num = 5, Name = "Май" },
-            new Month { Num = 6, Name = "Июнь" },
-            new Month { Num = 7, Name = "Июль" },
-            new Month { Num = 8, Name = "Август" },
-            new Month { Num = 9, Name = "Сентябрь" },
-            new Month { Num = 10, Name = "Октябрь" },
-            new Month { Num = 11, Name = "Ноябрь" },
-            new Month { Num = 12, Name = "Декабрь" }
+            new DropDownList { Num = 0, Name = "Месяц не выбран" },
+            new DropDownList { Num = 1, Name = "Январь" },
+            new DropDownList { Num = 2, Name = "Февраль" },
+            new DropDownList { Num = 3, Name = "Март" },
+            new DropDownList { Num = 4, Name = "Апрель" },
+            new DropDownList { Num = 5, Name = "Май" },
+            new DropDownList { Num = 6, Name = "Июнь" },
+            new DropDownList { Num = 7, Name = "Июль" },
+            new DropDownList { Num = 8, Name = "Август" },
+            new DropDownList { Num = 9, Name = "Сентябрь" },
+            new DropDownList { Num = 10, Name = "Октябрь" },
+            new DropDownList { Num = 11, Name = "Ноябрь" },
+            new DropDownList { Num = 12, Name = "Декабрь" }
         };
+        private readonly IEnumerable<DropDownList> _tableType = new List<DropDownList>
+        {
+            new DropDownList { Num = 0, Name = "Таблица из cdn.datatables.net" },
+            new DropDownList { Num = 1, Name = "Минималистичная таблица" }
+        };
+        
 
-        private IEnumerable<Year> AvailableYears()
+        private IEnumerable<DropDownList> AvailableYears()
         {
             var avalible = _allWeatherConditions.WeatherConditions.GroupBy(g => g.Date.Year);
-            List<Year> avalibleYears = new List<Year>();
-            avalibleYears.Add(new Year
+            List<DropDownList> avalibleYears = new List<DropDownList>();
+            avalibleYears.Add(new DropDownList
             {
-                YearNum = 0,
-                YearStr = "Год не выбран"
+                Num = 0,
+                Name = "Год не выбран"
             });
             foreach (var year in avalible)
             {
-                avalibleYears.Add(new Year
+                avalibleYears.Add(new DropDownList
                 {
-                    YearNum = year.Key,
-                    YearStr = year.Key.ToString()
+                    Num = year.Key,
+                    Name = year.Key.ToString()
                 });
             }
-            return avalibleYears.OrderBy(i=>i.YearNum);
+            return avalibleYears.OrderBy(i=>i.Num);
         }
 
         private readonly IAllWeatherConditions _allWeatherConditions;
@@ -66,16 +67,18 @@ namespace WeatherArchive.Controllers
         
         public IActionResult Checkout()
         {
-            ViewBag.SelectYears = new SelectList(AvailableYears(), "YearNum", "YearStr");
+            ViewBag.SelectYears = new SelectList(AvailableYears(), "Num", "Name");
             ViewBag.SelectMonths = new SelectList(_months, "Num", "Name");
+            ViewBag.SelectTableType = new SelectList(_tableType, "Num", "Name");
             return View();
         }
         
         [HttpPost]
         public IActionResult Checkout(WeatherConditionTableViewModel weather)
         {
-            ViewBag.SelectYears = new SelectList(AvailableYears(), "YearNum", "YearStr");
+            ViewBag.SelectYears = new SelectList(AvailableYears(), "Num", "Name");
             ViewBag.SelectMonths = new SelectList(_months, "Num", "Name");
+            ViewBag.SelectTableType = new SelectList(_tableType, "Num", "Name");
             WeatherConditionTableViewModel conditionObject = null;
             if (weather.Year!=0)
             {
@@ -83,6 +86,7 @@ namespace WeatherArchive.Controllers
                 {
                     conditionObject = new WeatherConditionTableViewModel()
                     {
+                        TableType = weather.TableType,
                         Year = weather.Year,
                         Month = weather.Month,
                         AllWeatherCondition = (from weatherCondition in _allWeatherConditions.WeatherConditions 
@@ -94,6 +98,7 @@ namespace WeatherArchive.Controllers
                 {
                     conditionObject = new WeatherConditionTableViewModel()
                     {
+                        TableType = weather.TableType,
                         Year = weather.Year,
                         Month = weather.Month,
                         AllWeatherCondition = (from weatherCondition in _allWeatherConditions.WeatherConditions 
@@ -106,6 +111,7 @@ namespace WeatherArchive.Controllers
             {
                 conditionObject = new WeatherConditionTableViewModel()
                 {
+                    TableType = weather.TableType,
                     Year = weather.Year,
                     Month = weather.Month,
                     AllWeatherCondition = _allWeatherConditions.WeatherConditions.OrderBy(i=>i.Date)
--- WeatherArchive/ViewModels/WeatherConditionTableViewModel.cs
+++ WeatherArchive/ViewModels/WeatherConditionTableViewModel.cs
@@ -5,6 +5,7 @@ namespace WeatherArchive.ViewModels
 {
     public class WeatherConditionTableViewModel
     {
+        public int TableType { get; set; }
         public ushort Year { get; set; }
         public ushort Month { get; set; }
         public IEnumerable<WeatherCondition> AllWeatherCondition { get; set; }
